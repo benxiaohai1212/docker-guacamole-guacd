023d3bd285f5d6f1bec7aaca04eb9748c37636d2
tunip <tunip@users.noreply.github.com>
2017-10-18 13:17:41 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 ln -sf /usr/lib/freerdp/guacdr-client.so /usr/lib/x86_64-linux-gnu/freerdp/guacdr-client.so
 ln -sf /usr/lib/freerdp/guacsnd-client.so /usr/lib/x86_64-linux-gnu/freerdp/guacsnd-client.so
 
-# Create directory
+# Create config directory for freerdp
 if [ ! -d /config/.config ]; then
    mkdir -p /config/.config
    chown -R duser:users /config
 fi
 
-exec su -s /bin/sh -c 'guacd -f -b 0.0.0.0' duser
+su-exec abc guacd -f -b 0.0.0.0
