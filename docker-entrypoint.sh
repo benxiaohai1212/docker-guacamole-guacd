53f4e57c187d0b206ae0c9f161f39bacea0e61d6
tunip <tunip@users.noreply.github.com>
2017-10-18 13:19:44 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# Set UID/GID
+PUID=${PUID:-911}
+PGID=${PGID:-911}
+
+# Set permissions
+groupmod -o -g "$PGID" abc
+usermod -o -u "$PUID" abc
+chown abc:abc /config
+
 # Link to the correct freerdp libs
 ln -sf /usr/lib/freerdp/guacdr-client.so /usr/lib/x86_64-linux-gnu/freerdp/guacdr-client.so
 ln -sf /usr/lib/freerdp/guacsnd-client.so /usr/lib/x86_64-linux-gnu/freerdp/guacsnd-client.so
